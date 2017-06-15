b205426f15412e73ffb4a32a7e52b580921d4cb8
Andre Heber <andre.heber@gmx.net>
2017-06-15 11:18:48 +0200

    test deploy 2

--- hello.js
+++ hello.js
@@ -1,21 +1,16 @@
-// Create server
+var express = require('express');
 var http = require('http');
-var server = http.createServer();
-
-// Create Gun
 var Gun = require('gun');
+var path = require('path');
+
+var port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8080;
+
+var app = express();
+app.use(Gun.serve);
+
+var server = app.listen(port);
 var gun = Gun({ web: server });
 
-server.listen(8080, function() {
-    console.log('Server listening on http://localhost:8080/gun')
-});
-
-var greetings = gun.get('greetings');
-
-greetings.put({
-    hello: 'world',
-});
-
-greetings.on(function (update) {
-    console.log('Update:', update);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
